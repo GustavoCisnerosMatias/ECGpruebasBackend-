15035d2a65355d5434fb35c6b89f1a9600adaa66
gustavo.cisnerosmatias <gustavo.cisnerosmatias@gmail.com>
2025-05-27 18:10:52 -0500

    change db to DB in the models

--- modelos/M_User.php
+++ modelos/M_User.php
@@ -4,7 +4,7 @@ class M_User extends \DB\SQL\Mapper
 {
     public function __construct()
     {
-        parent::__construct(\Base::instance()->get('db'), 'usuarios');
+        parent::__construct(\Base::instance()->get('DB'), 'usuarios');
     }
 
     public function getTodosusuarios()
